74a2a3a48b7e671a531ad4f2cad303a567b7857e
Kiet Vo <kiet.vo@logixtek.com>
2020-02-19 13:55:46 +0700

    Init new project

---
+++ src/Api/IdentityController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    [Route("identity")]
+
+    public class IdentityController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+
+        [HttpGet("test")]
+        public IActionResult Get2()
+        {
+            string test = "success";
+            return new JsonResult(test);
+        }
+    }
+}
